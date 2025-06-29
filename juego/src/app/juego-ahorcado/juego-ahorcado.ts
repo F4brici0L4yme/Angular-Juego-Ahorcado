679092a48d9947b4348ec3d1580fbe431c3ff485
MauroSullcaMamani <msullcam@unsa.edu.pe>
2025-06-29 18:24:04 -0500

    Implementado la lógica del juego de adivinar las letras

--- juego/src/app/juego-ahorcado/juego-ahorcado.ts
+++ juego/src/app/juego-ahorcado/juego-ahorcado.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AhorcadoService } from '../ahorcado-service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-juego-ahorcado',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './juego-ahorcado.html',
   styleUrl: './juego-ahorcado.css'
 })
 export class JuegoAhorcado {
+  letra = '';
+  constructor(public servicio: AhorcadoService, private router: Router) {}
 
+  probarLetra() {
+    this.servicio.adivinar(this.letra);
+    this.letra = '';
+    if (this.servicio.juegoTerminado) {
+      this.router.navigate(['/resultado']);
+    }
+  }
 }
