55d4b0bb022336bc8000c1ea50f9e996fc31e7e0
F4brici0L4yme <rlaymes@unsa.edu.pe>
2025-06-29 17:34:19 -0500

    componentes creados inicio - juego-ahorcado - resultado

---
+++ juego/src/app/juego-ahorcado/juego-ahorcado.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-juego-ahorcado',
+  imports: [],
+  templateUrl: './juego-ahorcado.html',
+  styleUrl: './juego-ahorcado.css'
+})
+export class JuegoAhorcado {
+
+}
