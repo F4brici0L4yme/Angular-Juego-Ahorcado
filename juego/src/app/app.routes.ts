c69c63c3b34a7968071391bbaf61bf9d1fa7d554
F4brici0L4yme <rlaymes@unsa.edu.pe>
2025-06-29 18:33:46 -0500

    app-routes agregadas - configuración de links completado

--- juego/src/app/app.routes.ts
+++ juego/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Inicio } from './inicio/inicio';
+import { JuegoAhorcado } from './juego-ahorcado/juego-ahorcado';
+import { Resultado } from './resultado/resultado';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Inicio },
+    { path: 'juego', component: JuegoAhorcado },
+    { path: 'resultado', component: Resultado },
+];
