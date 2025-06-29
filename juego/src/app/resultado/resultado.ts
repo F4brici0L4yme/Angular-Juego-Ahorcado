b3d19ed9cffbcb2e40d4b858e8d741cec580ba0e
MauroSullcaMamani <msullcam@unsa.edu.pe>
2025-06-29 18:26:52 -0500

    Definiendo la lógica para reiniciar la partida

--- juego/src/app/resultado/resultado.ts
+++ juego/src/app/resultado/resultado.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AhorcadoService } from '../ahorcado-service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-resultado',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './resultado.html',
   styleUrl: './resultado.css'
 })
 export class Resultado {
-
+  constructor(public servicio: AhorcadoService, private router: Router) {}
+  reiniciar() {
+    this.router.navigate(['/']);
+  }
 }
+
