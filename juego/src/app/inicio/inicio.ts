a1b45f51240a1f098597ea9be21003181cdc948d
MauroSullcaMamani <msullcam@unsa.edu.pe>
2025-06-29 18:19:17 -0500

    Agregando la lógica para iniciar juego con palabra ingresada

--- juego/src/app/inicio/inicio.ts
+++ juego/src/app/inicio/inicio.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AhorcadoService } from '../ahorcado-service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-inicio',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './inicio.html',
   styleUrl: './inicio.css'
 })
 export class Inicio {
-
+  palabra = '';
+  constructor(private servicio: AhorcadoService, private router: Router) {}
+  iniciarJuego() {
+    this.servicio.reiniciar(this.palabra);
+    this.router.navigate(['/juego']);
+  }
 }
